261d16fb77b98b01174157f67c38a8cd6a4841bb
Nour Sharabash <amirite@cpan.org>
2014-04-08 17:23:57 +0000

    Import of AMIRITE/Mojolicious-Plugin-NourConfig-0.02 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-NourConfig
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AMIRITE/Mojolicious-Plugin-NourConfig-0.02.tar.gz
    gitpan-cpan-author:       AMIRITE
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/NourConfig.pm
+++ lib/Mojolicious/Plugin/NourConfig.pm
@@ -6,11 +6,12 @@ has 'nour_config';
 
 sub register {
     my ( $self, $app, $opts ) = @_;
-    my $include_extra = delete $opts->{include_extra};
+    my $helpers = delete $opts->{ '-helpers' };
+    my $silence = delete $opts->{ '-silence' };
 
     $self->nour_config( new Nour::Config ( %{ $opts } ) );
 
-    if ( $include_extra ) { # inherit some helpers from Nour::Base
+    if ( $helpers ) { # inherit some helpers from Nour::Base
         do { my $method = $_; eval qq|
         \$app->helper( $method => sub {
             my ( \$ctrl, \@args ) = \@_;
@@ -22,12 +23,14 @@ sub register {
     my $current = $app->defaults( config => $app->config )->config;
     %{ $current } = ( %{ $current }, %{ $config } );
 
+    $app->log->debug( 'config', $app->dumper( $current ) ) unless $silence;
+
     return $current;
 }
 
 1;
 
-# ABSTRACT: imports config from a ./config directory full of nested yaml goodness
+# ABSTRACT: Imports config from a ./config directory full of nested YAML goodness.
 
 __END__
 
@@ -37,11 +40,76 @@ __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::NourConfig - imports config from a ./config directory full of nested yaml goodness
+Mojolicious::Plugin::NourConfig - Imports config from a ./config directory full of nested YAML goodness.
 
 =head1 VERSION
 
-version 0.01
+version 0.02
+
+=head1 USAGE / SYNOPSIS
+
+Place your YAML configuration files under a ./config sub-directory from your mojo app's home directory.
+There's an example in the package tarball you can look at, but roughly something like this:
+
+     $ find ./config/
+    ./config/
+    ./config/application
+    ./config/application/nested
+    ./config/application/nested/example.yml
+    ./config/application.yml
+    ./config/database
+    ./config/database/private
+    ./config/database/private/production.yml
+    ./config/database/private/README.md
+    ./config/database/config.yml
+
+Somewhere in your startup routine, include something like this:
+
+    $self->plugin( 'Mojolicious::Plugin::NourConfig', {
+        -base => 'config'
+        , -helpers => 1 # adds some unrelated helper methods i wrote
+        , -silence => 1 # turning this on disables the config dump on startup in the debug log
+    } );
+
+On application startup, if you haven't turned the silence option on you can see your configuration from the debug log:
+
+    [Tue Apr  8 12:10:21 2014] [debug] config
+    {
+      'application' => {
+        'nested' => {
+          'example' => {
+            'wow' => 'amazing'
+          }
+        },
+        'secret' => 'don\'t tell anyone'
+      },
+      'database' => {
+        'default' => {
+          'database' => 'production',
+          'option' => {
+            'AutoCommit' => '1',
+            'PrintError' => '1',
+            'RaiseError' => '1',
+            'pg_bool_tf' => '0',
+            'pg_enable_utf8' => '1'
+          },
+          'password' => 'nour',
+          'username' => 'nour'
+        },
+        'development' => {
+          'dsn' => 'dbi:Pg:dbname=nourdb_dev',
+          'password' => 'sharabash',
+          'username' => 'nour'
+        },
+        'production' => {
+          'dsn' => 'dbi:Pg:dbname=nourdb_prod;host=secret.com',
+          'password' => 'secret',
+          'username' => 'override'
+        }
+      }
+    }
+
+Neat, right? Yeah.
 
 =for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
 
